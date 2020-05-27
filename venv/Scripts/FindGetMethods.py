4592fe7fdf68000b907641e7aff2ddcc12ac757e
Armine.Movsisyan <ammovsisyan95@gmail.com>
2020-05-27 21:14:51 +0400

    add gitignore file

--- venv/Scripts/FindGetMethods.py
+++ venv/Scripts/FindGetMethods.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get("https://www.joomag.com/")
+driver.get("https://www.joomag.com/")git stash 
 elements = driver.find_elements_by_xpath('//*[contains(text(), ".get")]')
 driver.switch_to.frame('_hjRemoteVarsFrame')
 element = driver.find_element_by_xpath('//*[contains(text(), ".get")]')
